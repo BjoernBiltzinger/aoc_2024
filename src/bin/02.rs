dce8495b621b10255a5f51c9609f445a4734cb3d
Bjoern Biltzinger <bjoern@hema.to>
2024-12-02 10:45:28 +0100

    Day 2

---
+++ src/bin/02.rs
@@ -0,0 +1,98 @@
+advent_of_code::solution!(2);
+
+fn parse_input(input: &str) -> Vec<Vec<u32>> {
+    input
+        .lines()
+        .map(|line| {
+            let values = line.split_ascii_whitespace().map(|x| x.parse().ok().unwrap()).collect();
+            values
+        })
+        .collect()
+}
+
+fn check_valid(vec: &Vec<u32>, skip: usize) -> bool {
+    let increase: bool;
+
+    let start = if skip == 0 { 1 } else { 0 };
+    let second = if skip == 1 || skip == 0 { 2 } else { 1 };  
+
+    if vec[second] < vec[start] {
+        increase = false;
+    } else if vec[second] > vec[start] {
+        increase = true;    
+    } else {
+        return false;
+    }
+
+    let mut current_value = vec[start];
+
+    for (i,value) in vec.iter().enumerate() {
+        if i == skip || i == start {
+            continue;
+        }
+
+        if increase {
+            if *value < current_value{
+                return false;
+            }
+        } else {
+            if *value > current_value {
+                return false;
+            }
+        }
+        if value.abs_diff(current_value) > 3 {
+            return false;
+        }
+        if value.abs_diff(current_value) == 0 {
+            return false;
+        }
+        current_value = *value;
+    }
+    true
+}
+
+fn check_valid_part2(vec: &Vec<u32>) -> bool {
+    if check_valid(vec, 10000) {
+        return true;
+    }
+    for i in 0..vec.len() {
+        // drop i-th element
+
+        // first version with copy - new version avoids copying 
+        //let mut vec_copy = vec.clone();
+        //vec_copy.remove(i);
+        if check_valid(vec, i) {
+            return true;
+        }
+    }
+    false
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let vecs: Vec<Vec<u32>> = parse_input(input);
+
+    Some(vecs.iter().filter(|vec| check_valid(vec, 10000)).count() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let vecs: Vec<Vec<u32>> = parse_input(input);
+
+    Some(vecs.iter().filter(|vec| check_valid_part2(vec)).count() as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4));
+    }
+}
